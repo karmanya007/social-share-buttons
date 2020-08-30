701463571220323b39d225781c814e1043febce3
karmanya007 <karmanyasharma5@gmail.com>
2020-08-30 13:43:58 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,81 @@
+/* 
+
+Social Share Links:
+
+WhatsApp:
+https://wa.me/?text=[post-title] [post-url]
+
+Facebook:
+1. https://www.facebook.com/sharer.php?u=[post-url]
+
+<div id="fb-root"></div>
+  <script async defer crossorigin="anonymous" 
+        src="https://connect.facebook.net/en_US/sdk.js#xfbml=1
+             &version={graph-api-version}
+             &appId={your-facebook-app-id}
+             &autoLogAppEvents=1" 
+        nonce="FOKrbAYI">
+  </script>
+
+2.<div id="fb-root"></div>
+  <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v8.0"nonce="8U7nv0Uw"></script>
+  <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-width="50" data-layout="box_count" data-action="like" data-size="small" data-share="true"></div>
+  data-layout: standard, box_count, button_count, button
+  data-action: like, recommended
+
+Twitter:
+
+<script>window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));</script>
+
+1. https://twitter.com/share?url=[post-url]&text=[post-title]
+2. <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="hi" data-url="https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/overview" data-via="karmanyasharma5" data-hashtags="coding" data-related="shroud,asdasd" data-show-count="true">Tweet</a>
+   <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+3. <a href="https://twitter.com/karmanyasharma5?ref_src=twsrc%5Etfw" class="twitter-follow-button" data-size="large"
+        data-text="hi" data-url="https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/overview"
+        data-via="karmanyasharma5" data-hashtags="coding" data-related="shroud,asdasd" data-show-screen-name="false"
+        data-show-count="true">Follow @karmanyasharma5</a>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+4. <a href="https://twitter.com/messages/compose?recipient_id=713981765115707392&ref_src=twsrc%5Etfw"
+        class="twitter-dm-button" data-size="large" data-text="hi"
+        data-url="https://developer.twitter.com/en/docs/twitter-for-websites/tweet-button/overview"
+        data-via="karmanyasharma5" data-hashtags="coding" data-related="shroud,asdasd" data-show-screen-name="false"
+        data-screen-name="@karmanyasharma5" data-show-count="false">Message @@karmanyasharma5</a>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+Pinterest:
+
+<script type="text/javascript">
+(function(d){
+    var f = d.getElementsByTagName('SCRIPT')[0], p = d.createElement('SCRIPT');
+    p.type = 'text/javascript';
+    p.async = true;
+    p.src = '//assets.pinterest.com/js/pinit.js';
+    f.parentNode.insertBefore(p, f);
+}(document));
+</script>
+
+1. https://pinterest.com/pin/create/bookmarklet/?media=[post-img]&url=[post-url]&is_video=[is_video]&description=[post-title]
+2. <script type="text/javascript" async defer src="//assets.pinterest.com/js/pinit.js"></script>
+    <a href="https://www.pinterest.com/karmanyasharma187/" data-pin-do="buttonFollow">
+        Karmanya Sharma
+    </a>
+
+LinkedIn:
+https://www.linkedin.com/shareArticle?url=[post-url]&title=[post-title]
+
+*/
